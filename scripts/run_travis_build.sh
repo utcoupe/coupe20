220b8c3b3eb1af0802c2794217a3c999594d6905
Mindstan <mindstan@hotmail.fr>
2018-09-23 17:09:22 +0200

    :question: First test with travis

---
+++ scripts/run_travis_build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker run \
+    -i \
+    --mount type=bind,source="$(pwd)"/ros_ws/src,target=/utcoupe/coupe19/ros_ws/src,readonly \
+    --mount type=bind,source="$(pwd)"/libs,target=/utcoupe/coupe19/libs,readonly \
+    utcoupe/coupe19:utcoupe-ros-kinetic-amd64

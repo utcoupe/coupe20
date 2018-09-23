82bb68ce95d781ab8a00bd8d634037d0f37eae60
Mindstan <mindstan@hotmail.fr>
2018-09-23 17:33:29 +0200

    :bug: Fixed Read-only filesystem error

--- scripts/run_travis_build.sh
+++ scripts/run_travis_build.sh
@@ -2,7 +2,7 @@
 
 docker run \
     -i \
-    --mount type=bind,source="$(pwd)"/ros_ws/src,target=/utcoupe/coupe19/ros_ws/src,readonly \
+    --mount type=bind,source="$(pwd)"/ros_ws/src,target=/utcoupe/coupe19/ros_ws/src \
     --mount type=bind,source="$(pwd)"/libs,target=/utcoupe/coupe19/libs,readonly \
     utcoupe/coupe19:utcoupe-ros-kinetic-amd64 \
     /bin/bash -c "catkin_make install -DCMAKE_BUILD_TYPE=Release"

23e28707d450bdba4fb63aea77162de7933ca26d
MadeInPierre <pielaclau@gmail.com>
2018-12-01 15:08:04 +0100

    Cleaned message definitions and files

--- ros_ws/src/static_map/src/map_communication.py
+++ ros_ws/src/static_map/src/map_communication.py
@@ -10,9 +10,9 @@ from occupancy import OccupancyGenerator
 
 
 class Servers():
-    GET_CONTAINER_SERV        = "static_map/get_container"
-    GET_WAYPOINT_SERV         = "static_map/get_waypoint"
-    GET_TERRAIN_SERV          = "static_map/get_terrain"
+    GET_CONTAINER_SRV        = "static_map/get_container"
+    GET_WAYPOINT_SRV         = "static_map/get_waypoint"
+    GET_TERRAIN_SRV          = "static_map/get_terrain"
 
     SET_SERV        = "static_map/set"
     TRANSFER_SERV   = "static_map/transfer"
@@ -21,15 +21,14 @@ class Servers():
 
 class MapServices():
     def __init__(self, occupancy_generator):
-        self._get_container_srv = rospy.Service(Servers.GET_CONTAINER_SERV, static_map.srv.MapGetContainer, self.on_get_container)
-
+        self._get_container_srv = rospy.Service(Servers.GET_CONTAINER_SRV, static_map.srv.MapGetContainer, self.on_get_container)
+        self._get_waypoint_srv  = rospy.Service(Servers.GET_WAYPOINT_SRV, static_map.srv.MapGetWaypoint,   self.on_get_waypoint)
+        self._get_terrain_srv   = rospy.Service(Servers.GET_TERRAIN_SRV, static_map.srv.MapGetTerrain,     self.on_get_terrain)
         # self.GetSERV       = rospy.Service(Servers.GET_SERV, static_map.srv.MapGet,                self.on_get)
         # self.SetSERV       = rospy.Service(Servers.SET_SERV, static_map.srv.MapSet,                self.on_set) #TODO
         # self.TransferSERV  = rospy.Service(Servers.TRANSFER_SERV, static_map.srv.MapTransfer,      self.on_transfer)
         # self.OccupancySERV = rospy.Service(Servers.OCCUPANCY_SERV, static_map.srv.MapGetOccupancy, self.on_get_occupancy)
         # self.ObjectsSERV   = rospy.Service(Servers.OBJECTS_SERV, static_map.srv.MapGetObjects,     self.on_get_objects)
-        self.FillWPSERV    = rospy.Service(Servers.GET_WAYPOINT_SERV, static_map.srv.MapGetWaypoint,       self.on_get_waypoint)
-        # self.occupancy_generator = occupancy_generator
 
     def on_get_container(self, req):
         # Fetch it from the map
@@ -43,47 +42,6 @@ class MapServices():
         msg.success = True
         msg.container = self._create_container_msg(ct, req.include_subcontainers)
         rospy.loginfo("GET Container (path={}): {} object(s) returned.".format(req.path, len(msg.container.objects)))
-        return msg
-    
-    def _create_container_msg(self, ct, include_subcontainers):
-        msg = static_map.msg.MapContainer()
-        msg.name = ct.Name
-
-        for e in ct.Elements:
-            if isinstance(e, Container) and include_subcontainers is True:
-                c = self._create_container_msg(e, include_subcontainers)
-                msg.objects += c.objects
-            elif isinstance(e, Object):
-                msg.objects.append(self._create_object_msg(e))
-        return msg
-
-    def _create_object_msg(self, obj):
-        msg = static_map.msg.MapObject()
-        msg.name = ""
-        
-        if obj.Shape.Type == "rect":
-            msg.shape_type = msg.SHAPE_RECT
-            msg.width  = obj.Shape.Width
-            msg.height = obj.Shape.Height
-        elif obj.Shape.Type == "circle":
-            msg.shape_type = msg.SHAPE_CIRCLE
-            msg.radius = obj.Shape.Radius
-        elif obj.Shape.Type == "point":
-            msg.shape_type = msg.SHAPE_POINT
-        
-        msg.labels = obj.Labels
-        msg.color  = obj.Color.Name if obj.Color is not None else ""
-        return msg
-    
-    def _create_waypoint_msg(self, way):
-        msg = static_map.msg.Waypoint()
-        if way is not None:
-            msg.name = way.Name
-            msg.pose.x = way.Position.X
-            msg.pose.y = way.Position.Y
-            msg.pose.theta = way.Position.A
-            msg.has_angle = way.Position.HasAngle
-            msg.frame_id = way.Position.Frame
         return msg
     
     def on_get_waypoint(self, req):
@@ -105,7 +63,21 @@ class MapServices():
         return static_map.srv.MapGetWaypointResponse(success, self._create_waypoint_msg(w))
     
     def on_get_terrain(self, req):
-        pass
+        terrain = MapManager.get_terrain()
+
+        msg = static_map.srv.MapGetTerrainResponse()
+        msg.success = True
+        msg.shape = self._create_object_msg(terrain.Shape)
+        msg.layers = []
+
+        for l in terrain.Layers:
+            msg.layers.append(static_map.msg.MapLayer)
+            msg.layers[-1].name = l.Name
+            msg.layers[-1].walls = []
+
+            for w in l.Walls:
+                msg.layers[-1].walls.append(self._create_object_msg(w))
+        return msg
 
     # def on_get(self, req):
     #     s = time.time() * 1000
@@ -185,45 +157,44 @@ class MapServices():
         rospy.logdebug("    Process took {0:.2f}ms".format(time.time() * 1000 - s))
         return static_map.srv.MapGetOccupancyResponse(path)
 
-    def on_fill_waypoint(self, req):
-        s = time.time() * 1000
 
-        filled_waypoint, filled_waypoint_name = None, req.waypoint.name
-        if filled_waypoint_name is not None and filled_waypoint_name != '': # name was given, complete the rest
-            filled_waypoint = MapManager.get("/waypoints/{}/^".format(req.waypoint.name))
-        else: # assume pose was given, find the waypoint name
-            waypoints = MapManager.get("/waypoints/^").Dict
-            for w in waypoints:
-                if waypoints[w].get("position/x") == round(req.waypoint.pose.x, 3) and \
-                   waypoints[w].get("position/y") == round(req.waypoint.pose.y, 3):
-                    if req.waypoint.has_angle:
-                        if waypoints[w].get("position/a") == round(req.waypoint.pose.theta, 3):
-                            filled_waypoint      = waypoints[w]
-                            filled_waypoint_name = w
-                    else:
-                        filled_waypoint      = waypoints[w]
-                        filled_waypoint_name = w
-            if filled_waypoint is None:
-                rospy.logerr("    Request failed : could not find waypoint that corresponds to the given coordinates.")
+    def _create_container_msg(self, ct, include_subcontainers):
+        msg = static_map.msg.MapContainer()
+        msg.name = ct.Name
 
-        success, w = False, None
-        if filled_waypoint_name is not None and filled_waypoint is not None:
-            success = True
-            w = static_map.msg.Waypoint()
-            w.name = filled_waypoint_name
-            w.frame_id = filled_waypoint.get("position/frame_id")
-            w.pose.x, w.pose.y = filled_waypoint.get("position/x"), filled_waypoint.get("position/y")
+        for e in ct.Elements:
+            if isinstance(e, Container) and include_subcontainers is True:
+                c = self._create_container_msg(e, include_subcontainers)
+                msg.objects += c.objects
+            elif isinstance(e, Object):
+                msg.objects.append(self._create_object_msg(e))
+        return msg
 
-            w.has_angle = req.waypoint.has_angle
-            if req.waypoint.has_angle and "a" in filled_waypoint.Dict["position"].Dict:
-                w.pose.theta = filled_waypoint.get("position/a")
-            rospy.loginfo("FILL_WAYPOINT: {} ({}, {}{})".format(str(w.name),
-                                                                w.pose.x, w.pose.y,
-                                                                ", {}".format(
-                                                                    w.pose.theta) if w.has_angle else ""))
-
-        rospy.logdebug("    Responding: {} ({}, {}{})".format(filled_waypoint_name, filled_waypoint.get("position/x"), filled_waypoint.get("position/y"),
-                                                                  ", {}".format(w.pose.theta) if w.has_angle else ""))
-
-        rospy.logdebug("    Process took {0:.2f}ms".format(time.time() * 1000 - s))
-        return static_map.srv.MapGetWaypointResponse(success, w)
+    def _create_object_msg(self, obj):
+        msg = static_map.msg.MapObject()
+        msg.name = ""
+        
+        if obj.Shape.Type == "rect":
+            msg.shape_type = msg.SHAPE_RECT
+            msg.width  = obj.Shape.Width
+            msg.height = obj.Shape.Height
+        elif obj.Shape.Type == "circle":
+            msg.shape_type = msg.SHAPE_CIRCLE
+            msg.radius = obj.Shape.Radius
+        elif obj.Shape.Type == "point":
+            msg.shape_type = msg.SHAPE_POINT
+        
+        msg.labels = obj.Labels
+        msg.color  = obj.Color.Name if obj.Color is not None else ""
+        return msg
+    
+    def _create_waypoint_msg(self, way):
+        msg = static_map.msg.Waypoint()
+        if way is not None:
+            msg.name = way.Name
+            msg.pose.x = way.Position.X
+            msg.pose.y = way.Position.Y
+            msg.pose.theta = way.Position.A
+            msg.has_angle = way.Position.HasAngle
+            msg.frame_id = way.Position.Frame
+        return msg

880bdf6ac71a0e30d11c3e11f90d9d04e54613b7
Mindstan <mindstan@hotmail.fr>
2019-03-09 18:27:27 +0100

    :bug: Fixed map crash

--- ros_ws/src/static_map/src/map_manager/__init__.py
+++ ros_ws/src/static_map/src/map_manager/__init__.py
@@ -1,4 +1,3 @@
 from map import MapDict, MapManager
-from map_bases import SetMode, MapElement, RequestPath
 from map_classes import Terrain, Object, Container, Waypoint
 from map_attributes import Position2D

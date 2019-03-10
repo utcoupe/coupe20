865a4c157c52eab314075b75f748623009542232
Floriane ALLAIRE <floriane.allaire@etu.utc.fr>
2019-03-10 18:44:01 +0100

    messages arduino

--- arduino_tower/arduino_tower/arduino_tower.ino
+++ arduino_tower/arduino_tower/arduino_tower.ino
@@ -9,11 +9,9 @@
 //ROS includes
 #include <ros.h> 
 #include <game_manager/GameStatus.h>
-#include <ard_tower/TowerInit.h> 
-#include <ard_tower/TowerInitResponse.h>
-#include <ard_tower/TowerUnload.h>
-#include <ard_tower/TowerUnloadResponse.h>
-#include <ard_tower/TowerLoad.h> 
+#include <ard_tower/Tower.h> 
+#include <ard_tower/TowerResponses.h> 
+
 ros :: NodeHandle nh ; 
 
 // Actuators includes 
@@ -36,46 +34,81 @@ void on_game_status(const game_manager::GameStatus& msg){
   game_status = msg.game_status;
   nh.loginfo("game_status_msg");
 }
-
-void on_tower_init(const ard_tower::TowerInit& msg) { 
-  // tower_initialize(); 
-  nh.loginfo("tower_init_msg") ; 
+void on_tower(const ard_tower::Tower& msg){
+  nh.loginfo("tower_msg");
+  load_content    = msg.load_content ; 
+  load_content_nb = msg.load_content_nb ; 
+  unload_content  = msg.unload_content ; 
+  if (msg.init_status == 1 && game_status == 1) {
+    tower_initialize(); 
+  }
 }
-
-void on_tower_unload (const ard_tower::TowerUnload& msg){
-  unload_content = msg.unload_content ; 
-  nh.loginfo("tower_unload_msg") ;
-}
-
-//void on_tower_load  (const ard_tower::TowerLoad& msg){
-//  nh.loginfo("tower_load_msg") ;
-//  //load_content          = msg.load_content ; 
-//  //load_content_nb       = msg.load_content_nb ; 
-//  //load_content_position = msg.load_content_nb ; 
-//}
-
 
 // ~ Subscriber ~ 
-ros::Subscriber<game_manager::GameStatus> sub_game_status  ("ai/game_manager/status",     &on_game_status)  ;
-ros::Subscriber<ard_tower::TowerInit>     sub_tower_init   ("actuators/ard_tower/init",   &on_tower_init)   ;  
-ros::Subscriber<ard_tower::TowerUnload>   sub_tower_unload ("actuators/ard_tower/unload", &on_tower_unload) ;
-//ros::Subscriber<ard_tower::TowerLoad>     sub_tower_load   ("actuators/ard_tower/load",   &on_tower_load)   ;
+ros::Subscriber<game_manager::GameStatus> sub_game_status  ("ai/game_manager/status", &on_game_status)  ;
+ros::Subscriber<ard_tower::Tower>         sub_tower        ("actuators/ard_tower",    &on_tower)   ;  
 
 
 // ~ Publisher ~ 
-ard_tower::TowerInitResponse  init_event_msg ; 
-ard_tower::TowerUnloadResponse unload_event_msg ; 
-ros::Publisher pub_tower_init   ("actuators/ard_tower/init_event",   &init_event_msg); 
-ros::Publisher pub_tower_unload ("actuators/ard_tower/unload_event", &unload_event_msg); 
+ard_tower::TowerResponses  event_msg ; 
+ros::Publisher pub_tower_responses   ("actuators/ard_tower/event",   &event_msg); 
+
 
 
 // ------ MAIN FUNCTIONS ----- 
 
 void tower_initialize() { 
+  nh.loginfo("tower_initialize"); 
   servo_unload.write(POS_UNLOAD_INIT) ;   // initialize stepper 
-  init_event_msg.init_success = 1 ; 
-  pub_tower_init.publish(&init_event_msg); 
+  event_msg.init_success = 1 ; 
+  pub_tower_responses.publish(&event_msg); 
 }
+
+int move_lift(int wanted_position) {  // move lift to the correct floor 
+nh.loginfo("move_lift"); 
+  if ( lift_position > wanted_position && wanted_position >= H_GROUND && game_status == 1 ){  // go down 
+    nh.loginfo("goes down"); 
+    //stepper_load.moveStep(wanted_position,true) ; 
+    lift_position = wanted_position ; 
+    return 1 ; 
+  }
+
+  if (lift_position < wanted_position && wanted_position <= H_SAS_LOW && game_status == 1 ) { // go up 
+    nh.loginfo("goes up"); 
+    //stepper_load.moveStep(wanted_position,false) ; 
+    lift_position = wanted_position ; 
+    return 1 ; 
+  }
+
+  if (lift_position == wanted_position && game_status == 1) { //nothing 
+    nh.loginfo("do nothing)"); 
+    return 1 ; 
+  }
+
+  //TODO => sucess of the move before closing or opening AX12 
+  nh.logwarn("error in moving"); 
+  return 0 ; 
+}
+
+int lift_atoms_to_sas() { //bring up the rest of atoms to the sas 
+  nh.loginfo("lift_atoms_to_sas"); 
+  int y ; //number of atom not being brought up to the sas 
+  if (nb_atom_in > MAX_ATOM_SAS ) {
+    y = nb_atom_in - ( MAX_ATOM_SAS - nb_atom_in_sas ) ; 
+    nb_atom_in_sas = MAX_ATOM_SAS ; 
+  }
+  else {
+    y = 0 ; 
+    nb_atom_in_sas = nb_atom_in ; 
+  }
+  int h = (H_SAS_LOW-lift_position)-y*H_ATOM ; // or (y-1/2)  // h the  high that need to be brought up 
+  int wanted_position = lift_position + h ; 
+  int success = move_lift(wanted_position) ; 
+  //TODO open AX12 
+  return success ; 
+}
+
+// ~ unload ~ 
 
 void unload_atom() {
   int success ; 
@@ -85,9 +118,9 @@ void unload_atom() {
      if (( (nb_atom_in_sas == 0 && nb_atom_in == 0) || success == 0 ) && game_status == 1) {
       nh.loginfo("unload finished"); 
       //TODO clarify message if no success 
-      unload_event_msg.unload_success = success ;
-      unload_event_msg.nb_atom_out    = nb_atom_out ; 
-      pub_tower_unload.publish(&unload_event_msg) ; 
+      event_msg.unload_success = success ;
+      event_msg.nb_atom_out    = nb_atom_out ; 
+      pub_tower_responses.publish(&event_msg) ; 
       unload_content = 0 ; 
       nb_atom_out    = 0 ; 
     }
@@ -99,20 +132,19 @@ void unload_atom() {
 
     if ( (nb_atom_in-nb_atom_in_sas == 0 || success == 0) && game_status == 1 ) {
       nh.loginfo("unload finished") ; 
-      unload_event_msg.unload_success = success ; 
-      unload_event_msg.nb_atom_out    = nb_atom_out ; 
-      pub_tower_unload.publish(&unload_event_msg); 
+      event_msg.unload_success = success ; 
+      event_msg.nb_atom_out    = nb_atom_out ; 
+      pub_tower_responses.publish(&event_msg); 
       unload_content = 0 ; 
       nb_atom_out = 0 ;
     }
   }
 
-  
 }
 
 int unload_atom_slider() {
   nh.loginfo("unload_atom_slider") ; 
-  int success ; 
+  int success = 0 ; 
   if (nb_atom_in_sas > 0 && game_status == 1) { // sas not empty yet 
     nh.loginfo("atom in sas") ; 
     servo_unload.write(POS_UNLOAD); 
@@ -124,8 +156,9 @@ int unload_atom_slider() {
     success = 1 ; 
   }
 
-
-
+    if (nb_atom_in_sas == 0 && nb_atom_in > 0 && game_status == 1) {
+      success = lift_atoms_to_sas() ; // 1 good, 0 bad wasn't able to reload sas 
+    }
 
   return success ; 
 }
@@ -168,12 +201,9 @@ void setup() {
   nh.initNode(); 
 
   nh.subscribe(sub_game_status);
-  nh.subscribe(sub_tower_init); 
-  nh.subscribe(sub_tower_unload); 
-  //nh.subscribe(sub_tower_load); 
-
-  nh.advertise(pub_tower_init); 
-  nh.advertise(pub_tower_unload); 
+  nh.subscribe(sub_tower); 
+ 
+  nh.advertise(pub_tower_responses); 
 
   // Servo Actuator init 
   servo_unload.attach(servo_unload_pin) ; 

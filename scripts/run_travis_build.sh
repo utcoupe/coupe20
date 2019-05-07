67e4639352a70998477ec2e8db23d563a5b2e686
Mindstan <mindstan@hotmail.fr>
2019-05-07 15:53:48 +0200

    :wrench: Unit tests are compiled only on master and devel

--- scripts/run_travis_build.sh
+++ scripts/run_travis_build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+CATKIN_CUSTOM_PARAMS="-DCMAKE_BUILD_TYPE=Release"
+
+if [ "$TRAVIS_BRANCH" == "devel" || "$TRAVIS_BRANCH" == "master" ]; then
+    CATKIN_CUSTOM_PARAMS="-DUTCOUPE_BUILD_TESTS=true $CATKIN_CUSTOM_PARAMS"
+fi
+
 docker run \
     -i \
     --mount type=bind,source="$(pwd)"/ros_ws/src,target=/utcoupe/coupe19/ros_ws/src \
     --mount type=bind,source="$(pwd)"/libs,target=/utcoupe/coupe19/libs,readonly \
     utcoupe/coupe19:utcoupe-ros-kinetic-amd64 \
-    /bin/bash -c "catkin_make install -DCMAKE_BUILD_TYPE=Release"
+    /bin/bash -c "catkin_make install $CATKIN_CUSTOM_PARAMS"

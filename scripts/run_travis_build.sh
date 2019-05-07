110c917bf7e7b1a96cf3b6840e02356ea4f177d3
Mindstan <mindstan@hotmail.fr>
2019-05-07 17:08:07 +0200

    :bug: Tests compiled ?

--- scripts/run_travis_build.sh
+++ scripts/run_travis_build.sh
@@ -4,14 +4,16 @@ CATKIN_CUSTOM_PARAMS="-DCMAKE_BUILD_TYPE=Release"
 
 if [ "$TRAVIS_BRANCH" == "devel" ] || [ "$TRAVIS_BRANCH" == "master" ]; then
 	echo "Adding tests to catkin."
-	CATKIN_CUSTOM_PARAMS="-DUTCOUPE_BUILD_TESTS=true $CATKIN_CUSTOM_PARAMS"
+	CATKIN_CUSTOM_PARAMS="-DUTCOUPE_BUILD_TESTS:BOOL=ON $CATKIN_CUSTOM_PARAMS"
 fi
 
-echo "--> final command : catkin_make install $CATKIN_CUSTOM_PARAMS"
+COMPILE_COMMAND="catkin_make install $CATKIN_CUSTOM_PARAMS"
+
+echo "--> final command : $COMPILE_COMMAND"
 
 docker run \
     -i \
     --mount type=bind,source="$(pwd)"/ros_ws/src,target=/utcoupe/coupe19/ros_ws/src \
     --mount type=bind,source="$(pwd)"/libs,target=/utcoupe/coupe19/libs,readonly \
     utcoupe/coupe19:utcoupe-ros-kinetic-amd64 \
-    /bin/bash -c "catkin_make $CATKIN_CUSTOM_PARAMS  && catkin_make install $CATKIN_CUSTOM_PARAMS"
+    /bin/bash -c "$COMPILE_COMMAND"

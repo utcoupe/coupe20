7e5170122e066967c06aa18920ed92d0bf1a6950
Paul Constant <paul.constant@etu.utc.fr>
2019-12-07 16:42:19 +0100

    refactor: remove brushlessMotor.cpp

--- stm32_asserv/include/motor.h
+++ stm32_asserv/include/motor.h
@@ -6,14 +6,24 @@
 #ifndef MOTOR_H
 #define MOTOR_H
 
-#include "brushlessMotor.h"
+#include "parameters.h"
+#include "pins.h"
+#include "stm32f3xx_hal.h"
+#include "local_math.h"
 
 #define MOTOR_LEFT 1
 #define MOTOR_RIGHT 2
 
+#define LEFT_READY_SHIFT 1
+#define RIGHT_READY_SHIFT 2
+
+#define LEFT_READY (1<<LEFT_READY_SHIFT)
+#define RIGHT_READY (1<<RIGHT_READY_SHIFT)
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
 void set_pwm(int side, int pwm);
 
 void MotorsInit(void);
--- stm32_asserv/src/serial_sender.cpp
+++ stm32_asserv/src/serial_sender.cpp
@@ -12,6 +12,7 @@
 
 #include <stdint.h>
 #include <string.h>
+#include <stdio.h>
 
 extern Serial g_serial;
 

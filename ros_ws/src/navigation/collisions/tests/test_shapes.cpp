900d3babe33322bd2a6068f0df98d2c55731da69
Mindstan <mindstan@hotmail.fr>
2020-03-15 19:06:23 +0100

    :wrench: Added more tests circle/rect

--- ros_ws/src/navigation/collisions/tests/test_shapes.cpp
+++ ros_ws/src/navigation/collisions/tests/test_shapes.cpp
@@ -9,6 +9,14 @@
 #include <vector>
 #include <utility>
 
+template<class Shape>
+void checkAllCollidesWith(std::vector<Shape>& collidingShapes, CollisionsShapes::AbstractShape& specialShape) {
+    for (auto& shape: collidingShapes) {
+        REQUIRE( specialShape.isCollidingWith(shape) );
+        REQUIRE( shape.isCollidingWith(specialShape) );
+    }
+}
+
 SCENARIO( "First shape is a rectangle", "[shape][rectangle]" ) {
     GIVEN( "Rectangle is a square" ) {
         Position posRect {0.0, 0.0, 0.0};
@@ -32,10 +40,7 @@ SCENARIO( "First shape is a rectangle", "[shape][rectangle]" ) {
             };
             
             THEN( "They are colliding" ) {
-                for (auto& circ: collidingCircles) {
-                    REQUIRE( rect.isCollidingWith(circ) );
-                    REQUIRE( circ.isCollidingWith(rect) );
-                }
+                checkAllCollidesWith(collidingCircles, rect);
             }
         }
         
@@ -45,6 +50,28 @@ SCENARIO( "First shape is a rectangle", "[shape][rectangle]" ) {
                 CollisionsShapes::Circle circ(posRect + Point(0.0, 2.0), 0.9);
                 REQUIRE( !rect.isCollidingWith(circ) );
                 REQUIRE( !circ.isCollidingWith(rect) );
+            }
+        }
+        
+        WHEN( "There is a circle intersecting, with at least one vertex inside it" ) {
+            std::vector<CollisionsShapes::Circle> collidingCircles {
+                CollisionsShapes::Circle(posRect + Point(-1.5, 1.5), 1.0), // 1 vertex
+                CollisionsShapes::Circle(posRect + Point(1.5, 0.0), 2.0),  // 2 vertices
+                CollisionsShapes::Circle(posRect + Point(-1.5, -1.5), 3.0) // 3 vertices
+            };
+            
+            THEN( "They are colliding" ) {
+                checkAllCollidesWith(collidingCircles, rect);
+            }
+        }
+        
+        WHEN( "There is a circle intersecting, without any vertices inside it" ) {
+            std::vector<CollisionsShapes::Circle> collidingCircles {
+                CollisionsShapes::Circle(posRect + Point(0.0, 1.5), 1.0)
+            };
+            
+            THEN( "They are colliding" ) {
+                checkAllCollidesWith(collidingCircles, rect);
             }
         }
     }

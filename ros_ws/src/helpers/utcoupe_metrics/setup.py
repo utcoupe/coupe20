958c8f60e9df1069293f91c156f2130ba6199287
Mindstan <mindstan@hotmail.fr>
2020-03-29 17:27:50 +0200

    :bug: Fixed python submodule not installed

--- ros_ws/src/helpers/utcoupe_metrics/setup.py
+++ ros_ws/src/helpers/utcoupe_metrics/setup.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python
 ## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
 
 from distutils.core import setup
@@ -5,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['utcoupe_metrics'],
+    packages=['utcoupe_metrics', 'utcoupe_metrics.metric_types'],
     package_dir={'': 'src_py'})
 
 setup(**setup_args)
